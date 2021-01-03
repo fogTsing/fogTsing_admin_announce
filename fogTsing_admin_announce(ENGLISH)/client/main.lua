00430dd121b814caa1624369af2dde3eaadab090
fogTsing <65904185+fogTsing@users.noreply.github.com>
2021-01-03 10:02:32 +0800

    Update main.lua

--- fogTsing_admin_announce(ENGLISH)/client/main.lua
+++ fogTsing_admin_announce(ENGLISH)/client/main.lua
@@ -23,3 +23,12 @@ AddEventHandler("announce", function(args)
   ESX.Scaleform.ShowFreemodeMessage('~o~Moderator Announce', args, 5)
   args = nil
 end)
+
+RegisterNetEvent("send_no_perms_to_player")
+AddEventHandler("send_no_perms_to_player", function()
+  TriggerEvent("chat:addMessage", {
+    color = {255,0,0},
+    multiline = true,
+    args = {"[System]", "FOOL! YOU ARE NOT ADMIN!"}
+  })
+end)
