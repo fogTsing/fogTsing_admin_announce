dc8cc7144495beed20b1ef057a33b723a02ed97e
fogTsing <65904185+fogTsing@users.noreply.github.com>
2021-01-03 10:06:42 +0800

    Update main.lua

--- fogTsing_admin_announce(CHINESE)/server/main.lua
+++ fogTsing_admin_announce(CHINESE)/server/main.lua
@@ -2,12 +2,6 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-function ergodic(tab)
-    for k,v in pairs(tab) do
-        playerIdentifier = v
-    end
-end
-
 local adminSteamId = {
     "steam:你的Steam16进制码"
 }
